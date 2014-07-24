602a707bdfe9673a2b41ea7ddaf59bae74a84fec
Sam Wilson <sam@samwilson.id.au>
2014-07-24 14:47:36 +0800

    Update config example.

--- config.dist.php
+++ config.dist.php
@@ -1,19 +1,10 @@
 <?php
 
-class Hierarchive {
+$cookie_salt = 'cookie-salt-here';
 
-	public static $version = '0.0.1';
-
-	public static $cookie_salt = 'cookie-salt-here';
-
-	public static $skin = 'default';
-
-	public static $db_hostname = 'localhost';
-
-	public static $db_database = 'hierarchive';
-
-	public static $db_username = 'root';
-
-	public static $db_password = '';
-
-}
+$db_config = array(
+	'hostname' => 'localhost',
+	'database' => 'hierarchive',
+	'username' => 'root',
+	'password' => ''
+);
